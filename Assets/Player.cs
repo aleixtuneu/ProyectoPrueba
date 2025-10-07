59125b4803335a2170e8982fd9a626c2b37023f6
Yeray <yeray.rodriguez.7e5@itb.cat>
2025-10-07 16:24:00 +0200

    C

---
+++ Assets/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//tu madre tiene una polla que ya la quisiera yo me dio pena por tu padre el dia que se entero que fue la noche de bodas quien se lo iba a imaginar que iba a ser a a tu padre que lo iban a encular transexual transexual tu madre tiene muy grande el pene no se porqueeeee nunca mas te podras sentar cuando te encule por detras tu madre tiene muy grande el pene es un transexual
